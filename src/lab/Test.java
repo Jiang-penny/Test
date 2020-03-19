8e347cb5c4c35331ab4c330669407f9d29d6d6d4
PennyJiang <PennyJiang@asus>
2020-03-20 01:19:12 +0800

    homework request

--- src/lab/Test.java
+++ src/lab/Test.java
@@ -2,9 +2,10 @@ package lab;
 
 public class Test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-	}
+  public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	System.out.println("Hello everyone,");
+	System.out.print(" I am studying at Feng Chia University.");
+  }
 
 }
